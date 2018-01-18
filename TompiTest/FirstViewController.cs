c2dc4fdd3725c1d2b4b6d5c6fe2b3cc55cdde05f
Thomas Haukland <thomas.haukland@webstep.no>
2018-01-18 10:17:23 +0100

    It works...

---
+++ TompiTest/FirstViewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Foundation;
+using Plugin.DeviceOrientation;
+using UIKit;
+
+namespace TompiTest
+{
+    public class FirstViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            //UIDevice.CurrentDevice.SetValueForKey(new NSNumber((int)UIInterfaceOrientation.Portrait), new NSString("orientation"));
+            base.ViewDidLoad();
+
+            var _btn = new UIButton();
+            _btn.SetTitle("Gå til side 2", UIControlState.Normal);
+            _btn.TitleLabel.Font = UIFont.FromName("Helvetica", 30);
+            _btn.TitleLabel.TextAlignment = UITextAlignment.Center;
+            _btn.Frame = View.Frame;
+            _btn.TitleLabel.SizeToFit();
+            _btn.TouchUpInside += (sender, e) => 
+            {
+                PresentViewController(new SecondViewController(), false, null);
+            };
+            View.AddSubview(_btn);
+        }
+
+        public override UIInterfaceOrientation PreferredInterfaceOrientationForPresentation()
+        {
+            return UIInterfaceOrientation.Portrait;
+        }
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations()
+        {
+            return UIInterfaceOrientationMask.Portrait;
+        }
+
+        public override bool ShouldAutorotate()
+        {
+            return true;
+        }
+
+    }
+}
---
+++ TompiTest/SecondViewController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CoreFoundation;
+using Foundation;
+using UIKit;
+
+namespace TompiTest
+{
+    public class SecondViewController : UIViewController
+    {
+        private UIButton _btn;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _btn = new UIButton();
+            _btn.SetTitle("Gå til side 1", UIControlState.Normal);
+            _btn.TitleLabel.Font = UIFont.FromName("Helvetica", 30);
+            _btn.TitleLabel.TextAlignment = UITextAlignment.Center;
+            _btn.Frame = View.Frame;
+            _btn.TitleLabel.SizeToFit();
+            _btn.TouchUpInside += (sender, e) =>
+            {
+                DismissViewController(true, null);
+            };
+            View.AddSubview(_btn);
+        }
+
+        public override void ViewWillLayoutSubviews()
+        {
+            _btn.Frame = View.Frame;
+            base.ViewWillLayoutSubviews();
+        }
+
+        public override UIInterfaceOrientation PreferredInterfaceOrientationForPresentation()
+        {
+            return UIInterfaceOrientation.LandscapeLeft;
+        }
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations()
+        {
+            return UIInterfaceOrientationMask.LandscapeLeft;
+        }
+
+        public override bool ShouldAutorotate()
+        {
+            return true;
+        }
+
+    }
+}
